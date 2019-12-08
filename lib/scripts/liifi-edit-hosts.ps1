b25d6c6f68d84c2a9b081a1e44e7d3cb0c105408
Julio Stanley <code@juliostanley.com>
2019-12-08 08:02:48 -0500

    Processing update revision

--- lib/scripts/liifi-edit-hosts.ps1
+++ lib/scripts/liifi-edit-hosts.ps1
@@ -1,2 +1,2 @@
-Write-Host "Opening /etc/hosts of windows"
+Write-Host "Opening /etc/hosts of windows. Make sure to save. Changes should be immediate"
 sudo notepad C:/Windows/System32/drivers/etc/hosts

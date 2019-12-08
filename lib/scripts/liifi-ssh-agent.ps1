0a01b547f749486129b9a0bb5677e1aad02ae9fb
Julio Stanley <code@juliostanley.com>
2019-12-08 08:36:35 -0500

    Processing update revision

--- lib/scripts/liifi-ssh-agent.ps1
+++ lib/scripts/liifi-ssh-agent.ps1
@@ -6,7 +6,9 @@ sudo Start-Service ssh-agent
 $null = New-Item "$env:USERPROFILE/.ssh" -Type Directory -ErrorAction Ignore
 ssh-keygen -f "$env:HOME/.ssh/id_rsa"
 try { Get-Content "$env:HOME\.ssh\id_rsa.pub" | clip } catch { }
-Write-Host "Go to https://github.com/settings/ssh, then press 'Add SSH Key', and then ctrl+v (the key is already on your clipboard)" -foregroundcolor "yellow"
 
-ssh-agent
-ssh-agent -l
+ssh-add -D
+ssh-add
+ssh-add -l
+
+Write-Host "Go to https://github.com/settings/ssh, then press 'Add SSH Key', and then ctrl+v (the key is already on your clipboard)" -ForegroundColor Yellow

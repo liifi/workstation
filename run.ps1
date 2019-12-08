c7203b20f9eec012cb7fb610bf9ac96f0aa56e6e
Julio Stanley <code@juliostanley.com>
2019-12-08 01:39:19 -0500

    Processing update revision

--- run.ps1
+++ run.ps1
@@ -4,6 +4,6 @@ switch ($args[0]) {
   "revision" { . "$PSScriptRoot/lib/scripts/_update-revision.ps1" }
   "update" { ./run.ps1 revision; git push; }
   Default {
-    Write-Host "Usage: ./run.ps1 <revision|push>" -ForegroundColor Yellow
+    Write-Host "Usage: ./run.ps1 <revision|update>" -ForegroundColor Yellow
   }
 }
